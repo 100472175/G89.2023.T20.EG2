730f914bf76bcb2796ec95b1f01835d5848cec11
SrS3R6IO <100472343@alumnos.uc3m.es>
2023-02-08 18:03:24 +0100

    Function ValidateEAN13 done.

--- UC3MLogistics/OrderManager.py
+++ UC3MLogistics/OrderManager.py
@@ -2,6 +2,9 @@ import json
 from .OrderMangementException import OrderManagementException
 from .OrderRequest import OrderRequest
 
+"Used to get the nearest number to the sum of the 12 elements of the barcode with .ceil"
+import math
+
 class OrderManager:
     def __init__(self):
         pass
@@ -9,7 +12,14 @@ class OrderManager:
     def ValidateEAN13( self, eAn13 ):
         # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
         # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
-        return True
+        check_sum = 0
+        for i in range(len(eAn13)-1):
+            if eAn13[i] % 2 != 0:
+                check_sum += eAn13[i]*3
+            else:
+                check_sum += eAn13[i]
+        difference = 10*math.ceil(check_sum%10) - check_sum
+        return eAn13[-1] == difference
 
     def ReadproductcodefromJSON( self, fi ):
 
