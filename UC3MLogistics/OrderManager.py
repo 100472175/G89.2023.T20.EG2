017d98ce6fc66309652fd5f09db87695db78ae65
100472175 <100472175@alumnos.uc3m.es>
2023-02-22 18:57:42 +0100

    OrderManager.py fixed v1

--- UC3MLogistics/OrderManager.py
+++ UC3MLogistics/OrderManager.py
@@ -1,46 +1,56 @@
 import json
+#Used to get the nearest number to the sum of the 12 elements of the barcode with .ceil
+import math
 from .OrderMangementException import OrderManagementException
 from .OrderRequest import OrderRequest
 
-#Used to get the nearest number to the sum of the 12 elements of the barcode with .ceil
-import math
 
 class OrderManager:
+    """
+    This is the class that manages the orders.
+    It is in charge of reading the JSON file.
+    """
     def __init__(self):
         pass
 
-    def ValidateEAN13( self, eAn13):
-        # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
-        # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
-        check_sum = 0
-        for i in range(len(eAn13)-1):
-            current_number = int(eAn13[i])
+    def validate_ean13(self, ean13):
+        """
+        This function validates the EAN13 code.
+        :param eAn13:
+        :return:
+        """
+        CheckSum = 0
+        for i in range(len(ean13)-1):
+            CurrentNumber = int(ean13[i])
             if i % 2 != 0:
-                check_sum += current_number*3
+                CheckSum += CurrentNumber*3
             else:
-                check_sum += current_number
-        difference = 10*math.ceil(check_sum/10) - check_sum
-        return int(eAn13[-1]) == difference
+                CheckSum += CurrentNumber
+        Difference = 10*math.ceil(CheckSum/10) - CheckSum
+        return int(ean13[-1]) == Difference
 
-    def ReadproductcodefromJSON( self, fi ):
+    def read_product_code_from_json( self, fi ):
+        """
+        This function reads the product code from the JSON file.
+        And raises an exception if the file is not found or the JSON is not valid.
+        """
+        try:
+            with open(fi, encoding='utf-8') as File:
+                Data = json.load(File)
+        except FileNotFoundError as Elem:
+            raise OrderManagementException("Wrong file or file path") from Elem
+        except json.JSONDecodeError as Elem:
+            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from Elem
+
 
         try:
-            with open(fi) as f:
-                DATA = json.load(f)
-        except FileNotFoundError as e:
-            raise OrderManagementException("Wrong file or file path") from e
-        except json.JSONDecodeError as e:
-            raise OrderManagementException("JSON Decode Error - Wrong JSON Format") from e
-
-
-        try:
-            PRODUCT = DATA["id"]
-            PH = DATA["phoneNumber"]
-            req = OrderRequest(PRODUCT, PH)
-        except KeyError as e:
-            raise OrderManagementException("JSON Decode Error - Invalid JSON Key") from e
-        if not self.ValidateEAN13(PRODUCT):
+            PRODUCT = Data["id"]
+            PhP = Data["phoneNumber"]
+            ReqOrd = OrderRequest(PRODUCT, PhP)
+        except KeyError as Elem:
+            raise OrderManagementException("JSON Decode Error - Invalid JSON Key") from Elem
+        if not self.validate_ean13(PRODUCT):
             raise OrderManagementException("Invalid PRODUCT code")
 
         # Close the file
-        return req
+        return ReqOrd
