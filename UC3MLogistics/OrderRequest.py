e27303f977412f1157b29b6d25956403df3e76b6
SrS3R6IO <100472343@alumnos.uc3m.es>
2023-02-22 19:20:49 +0100

    Updated OrderRequest.py to follow the standards of pylint

--- UC3MLogistics/OrderRequest.py
+++ UC3MLogistics/OrderRequest.py
@@ -1,27 +1,48 @@
+# pylint: disable=missing-module-docstring
 import json
 from datetime import datetime
 
 
 class OrderRequest:
+    """
+    This class is for creating a request latter implemented on OrderManager
+    It also implements properties and setters
+    """
     def __init__( self, idcode, phoneNumber ):
-        self.__phoneNumber = phoneNumber
-        self.__idcode = idcode
-        justnow = datetime.utcnow()
-        self.__timeStamp = datetime.timestamp(justnow)
+        self.__phone_number = phoneNumber
+        self.__id_code = idcode
+        JustNow = datetime.utcnow()
+        self.__time_stamp = datetime.timestamp(JustNow)
 
     def __str__(self):
         return "OrderRequest:" + json.dumps(self.__dict__)
 
     @property
-    def Phone( self ):
-        return self.__phoneNumber
-    @Phone.setter
-    def Phone( self, value ):
-        self.__phoneNumber = value
+    def phone( self ):
+        """
+        property of phone Number
+        :return:
+        """
+        return self.__phone_number
+    @phone.setter
+    def phone( self, value ):
+        """
+        corresponding setter of phone Number
+        :param value: the hidden value
+        """
+        self.__phone_number = value
 
     @property
-    def PRODUCT_CODE( self ):
-        return self.__idcode
-    @PRODUCT_CODE.setter
-    def PRODUCT_CODE( self, value ):
-        self.__idcode = value
+    def product_code( self ):
+        """
+        property of product code
+        :return:
+        """
+        return self.__id_code
+    @product_code.setter
+    def product_code( self, value ):
+        """
+        corresponding setter of product code
+        :param value: the hidden value
+        """
+        self.__id_code = value
