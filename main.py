3b02ee14e11ccb95b9dcc7d432827d0f569bb816
SrS3R6IO <100472343@alumnos.uc3m.es>
2023-02-22 19:00:38 +0100

    Updated main to follow the standards of pylint

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from UC3MLogistics import OrderManager
+"""
+Author: UC3M
+MODIFIED BY TEAM 20 (Eduardo Alarcón and Sergio Barragán)
+"""
 import string
 from barcode import EAN13
 from barcode.writer import ImageWriter
+from UC3MLogistics import OrderManager
 
 #GLOBAL VARIABLES
 letters = string.ascii_letters + string.punctuation + string.digits
@@ -9,39 +13,54 @@ shift = 3
 
 
 def Encode(word):
-    encoded = ""
-    for letter in word:
-        if letter == ' ':
-            encoded = encoded + ' '
+    """
+    This functions encodes a number
+    :param word: EAn13 number
+    :return: string
+    """
+    Encoded = ""
+    for Letter in word:
+        if Letter == ' ':
+            Encoded = Encoded + ' '
         else:
-            x = (letters.index(letter) + shift) % len(letters)
-            encoded = encoded + letters[x]
-    return encoded
+            Xvariable = (letters.index(Letter) + shift) % len(letters)
+            Encoded = Encoded + letters[Xvariable]
+    return Encoded
 
 def Decode(word):
-    encoded = ""
-    for letter in word:
-        if letter == ' ':
-            encoded = encoded + ' '
+    """
+    This function decode the EAn13 Number
+    :return: string
+    """
+    Encoded = ""
+    for Letter in word:
+        if Letter == ' ':
+            Encoded = Encoded + ' '
         else:
-            x = (letters.index(letter) - shift) % len(letters)
-            encoded = encoded + letters[x]
-    return encoded
+            Xvariable = (letters.index(Letter) - shift) % len(letters)
+            Encoded = Encoded + letters[Xvariable]
+    return Encoded
 
-def main():
-    mng = OrderManager()
-    res = mng.ReadproductcodefromJSON("test.json")
-    strRes = res.__str__()
-    print(strRes)
-    EncodeRes = Encode(strRes)
+def Main():
+    """
+    main functin of the
+    encode / deconde function for EAn13 numbers
+    :return:
+    """
+    Mng = OrderManager()
+    Res = Mng.ReadproductcodefromJSON("test.json")
+    StrRes = str(Res)
+    #StrRes = Res.__str__()
+    print(StrRes)
+    EncodeRes = Encode(StrRes)
     print("Encoded Res "+ EncodeRes)
     DecodeRes = Decode(EncodeRes)
     print("Decoded Res: " + DecodeRes)
-    print("Codew: " + res.PRODUCT_CODE)
-    with open("barcodeEan13.jpg", 'wb') as f:
-        iw = ImageWriter()
-        EAN13(res.PRODUCT_CODE, writer=iw).write(f)
+    print("Codew: " + Res.PRODUCT_CODE)
+    with open("barcodeEan13.jpg", 'wb') as Stored:
+        Imagew = ImageWriter()
+        EAN13(Res.PRODUCT_CODE, writer=Imagew).write(Stored)
 
 
 if __name__ == "__main__":
-    main()
+    Main()
